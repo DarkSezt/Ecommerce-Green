8862e577a835a67ab0fbde783446e1148c086262
Juaisbel Flores <juaisbel.flores@gmail.com>
2023-02-28 23:36:49 -0400

    Finishing home page with the css, html and the script

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Script for navigation bar
+const bar = document.getElementById("bar")
+const nav = document.getElementById('navbar')
+const closes = document.getElementById('close')
+
+if(bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active')
+    })
+}
+if(closes){
+    closes.addEventListener('click', ()=>{
+        nav.classList.remove('active')
+    })
+}
+
+
